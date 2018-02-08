c6891797143178640b858efe4d6216535059d518
Derek Stavis <dekestavis@gmail.com>
2018-02-08 18:41:45 -0200

    template: refactor to add demo links and navigation

--- template/src/App.js
+++ template/src/App.js
@@ -3,41 +3,32 @@ import React from 'react'
 import {
   ThemeProvider,
   Typeset,
-  Layout,
 } from 'former-kit'
 import theme from 'former-kit-skin-pagarme'
 
-import { HashRouter, Route } from 'react-router-dom'
+import {
+  HashRouter,
+  Route,
+  Switch,
+} from 'react-router-dom'
 
-import Sidebar from './containers/Sidebar'
-import Header from './containers/Header'
-import Footer from './containers/Footer'
-import Home from './pages/Home'
 import Login from './pages/Login'
+import LoggedArea from './pages/LoggedArea'
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <HashRouter>
       <Typeset>
         <div>
-          <Route
-            path="/login"
-            component={Login}
-          />
-          <Route
-            path="/home"
-            component={
-              () => (
-                <Layout
-                  sidebar={<Sidebar />}
-                  header={<Header />}
-                  footer={<Footer />}
-                >
-                  <Home />
-                </Layout>
-              )
-            }
-          />
+          <Switch>
+            <Route
+              path="/login"
+              component={Login}
+            />
+            <Route
+              component={LoggedArea}
+            />
+          </Switch>
         </div>
       </Typeset>
     </HashRouter>
