488a0258b6b4eadb9a34de4bbed04563e42ff0a9
Derek Stavis <dekestavis@gmail.com>
2018-02-11 17:36:37 -0200

    template: add i18n with react-i18next

--- template/src/index.js
+++ template/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import 'normalize.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import './initializeI18n'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
