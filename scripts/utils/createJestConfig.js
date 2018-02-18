d19f059388d558a26f33bd51b0da3df9de35a771
Derek Stavis <dekestavis@gmail.com>
2018-02-18 19:29:10 -0300

    chore: add enzyme test helper

--- scripts/utils/createJestConfig.js
+++ scripts/utils/createJestConfig.js
@@ -22,7 +22,10 @@ module.exports = (resolve, rootDir, isEjecting) => {
   // in Jest configs. We need help from somebody with Windows to determine this.
   const config = {
     collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
-    setupFiles: [resolve('config/polyfills.js')],
+    setupFiles: [
+      resolve('config/polyfills.js'),
+      resolve('config/jest/enzymeAdapter.js'),
+    ],
     setupTestFrameworkScriptFile: setupTestsFile,
     testMatch: [
       '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}',
